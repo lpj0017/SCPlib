1d8462a3edbe046ace807d3e9c8e1115b5efb076
Simone <simone.aonzo@gmail.com>
2015-12-01 11:32:25 +0100

    initial add

---
+++ app/src/main/java/it/unige/dibris/cseclab/scplib/ScpActivity.java
@@ -0,0 +1,22 @@
+package it.unige.dibris.cseclab.scplib;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class ScpActivity extends Activity
+{
+	public void startActivity(ScpIntent intent)
+	{
+
+	}
+
+    public void startActivityForResult(ScpIntent intent, int rc)
+	{
+
+	}
+
+}
---
+++ app/src/main/java/it/unige/dibris/cseclab/scplib/ScpIntent.java
@@ -0,0 +1,37 @@
+package it.unige.dibris.cseclab.scplib;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+public class ScpIntent extends Intent
+{
+	public static final String ACTION_SCP = "it.unige.scp.action";
+
+    public ScpIntent() {
+        super();
+    }
+
+    public ScpIntent(Intent o) {
+        super(o);
+    }
+
+    public ScpIntent(String action) {
+        super(action);
+    }
+
+    public ScpIntent(String action, Uri uri) {
+        super(action, uri);
+    }
+
+    public ScpIntent(Context packageContext, Class<?> cls) {
+        super(packageContext, cls);
+    }
+
+    public ScpIntent(String action, Uri uri, Context packageContext, Class<?> cls) {
+        super(action,uri,packageContext,cls);
+    }
+
+
+}
